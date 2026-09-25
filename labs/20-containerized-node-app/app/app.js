785932c390edc44efae871f753a0a66a94ed07ed
nathanprsnn6 <nathan.personeni@etu.univ-smb.fr>
2026-09-25 07:14:55 +0000

    feat: Initialize Docker and DB

--- labs/20-containerized-node-app/app/app.js
+++ labs/20-containerized-node-app/app/app.js
@@ -24,11 +24,11 @@ export async function buildApp() {
   // Get all quotes endpoint
   app.get("/", async (_req, reply) => {
     /******* TODO SELECT quotes from DB ******/
-    // const result = await app.pg.query('SELECT * FROM quotes')
-    // const quotes = result.rows;
+    const result = await app.pg.query('SELECT * FROM quotes')
+    const quotes = result.rows;
 
     // Placeholder test quotes
-    const quotes = [{text: "quote 1", author: "author 1"}, {text: "quote 2", author: "author 2"}]; 
+    //const quotes = [{text: "quote 1", author: "author 1"}, {text: "quote 2", author: "author 2"}]; 
     return reply.view("index.hbs", { quotes });
   });
 
@@ -43,7 +43,7 @@ export async function buildApp() {
     }
 
     /******* TODO INSERT quote into DB ******/
-    // await app.pg.query('INSERT INTO quotes (author, text) VALUES ($1, $2)', [author || "anonymous", text]);
+    await app.pg.query('INSERT INTO quotes (author, text) VALUES ($1, $2)', [author || "anonymous", text]);
 
     app.log.info({quote: { author: author || "anonymous", text }}, 'New quote added');
 
